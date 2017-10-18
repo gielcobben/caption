e1f80010ff3672726e98b1b07e9d07ad71a9e1cb
Giel Cobben <g.cobben@gmail.com>
2017-10-18 15:26:44 +0200

    Add tests

---
+++ renderer/components/__tests__/Content.test.js
@@ -0,0 +1,18 @@
+import ReactTestRenderer from "react-test-renderer";
+import Content from "./../Content";
+
+describe("<Content />", () => {
+  it("should show <ListEmpty /> when there is a query but no results", () => {
+    const tree = ReactTestRenderer.create(
+      <Content searchQuery="test" results={[]} />
+    );
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  it("should show <Drop /> when there is no searchQuery and no files dropped", () => {
+    const tree = ReactTestRenderer.create(
+      <Content searchQuery="" files={[]} />
+    );
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
