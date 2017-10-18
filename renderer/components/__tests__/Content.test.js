e066a3a414ef174ddc9f1212c40b6258510fb1cb
Vernon de Goede <vernon@ecurring.nl>
2017-10-18 15:33:18 +0200

    Mock react-dropzone module

--- renderer/components/__tests__/Content.test.js
+++ renderer/components/__tests__/Content.test.js
@@ -1,17 +1,19 @@
 import ReactTestRenderer from "react-test-renderer";
 import Content from "./../Content";
 
+jest.mock("react-dropzone", () => 'FakeDropzone');
+
 describe("<Content />", () => {
   it("should show <ListEmpty /> when there is a query but no results", () => {
     const tree = ReactTestRenderer.create(
-      <Content searchQuery="test" results={[]} />
+      <Content searchQuery="test" results={[]} />,
     );
     expect(tree.toJSON()).toMatchSnapshot();
   });
 
   it("should show <Drop /> when there is no searchQuery and no files dropped", () => {
     const tree = ReactTestRenderer.create(
-      <Content searchQuery="" files={[]} />
+      <Content searchQuery="" files={[]} />,
     );
     expect(tree.toJSON()).toMatchSnapshot();
   });
